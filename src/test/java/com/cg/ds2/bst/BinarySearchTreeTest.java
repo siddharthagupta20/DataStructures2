122ee8b36b133a0a0c3d880dd922a569976d7032
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-16 22:31:38 +0530

    [Siddhartha] modified: bst size checked

--- src/test/java/com/cg/ds2/bst/BinarySearchTreeTest.java
+++ src/test/java/com/cg/ds2/bst/BinarySearchTreeTest.java
@@ -12,6 +12,25 @@ public class BinarySearchTreeTest {
 		binaryTree.add(70);
 		Assert.assertEquals(3, binaryTree.getSize());
 	}
+	@Test
+	public void allNumbersAddedToTreeReturnSize() {
+		BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(65);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		Assert.assertEquals(13, binaryTree.getSize());
+	}
+	
 	
 	
 }
